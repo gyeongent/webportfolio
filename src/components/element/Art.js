2e92eab574ee9ed30d6cd42c5e4b14401b83e018
GYEONG <leegyeongmin@outlook.com>
2021-08-28 17:49:37 +0900

    2021/8/28-1

--- src/components/element/Art.js
+++ src/components/element/Art.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 import Youtube from "react-youtube";
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
 
 import testimage from "../../images/Arts/testimage.png"
 import distanceofscent from "../../images/Arts/distanceofscent.png"
@@ -18,105 +22,120 @@ import tretion from "../../images/Arts/tretion_1000.png"
 
 import "./Art.css";
 
-class ArtBoxDefault extends Component {
-    render(){
-        return(
-            <div className="ArtBox">
-                <a href={this.props.url} target="_blank">
-                    <img src={this.props.img} width="273px" />
-                </a>
-                <div>
-                    <p className="atitle">
-                        제목 : {this.props.title}
-                    </p>
-                    <p className="atitle">
-                        프로그램 : {this.props.program}
-                    </p>
-                    <p className="atitle">
-                        작업기간 : {this.props.length}
-                    </p>
-                    <p className="adescp">
-                        {this.props.adesc}
-                    </p>
-                </div>
+const useStyles = makeStyles((theme) => ({
+    modal: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    paper: {
+        backgroundColor: theme.palette.background.paper,
+        border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(1, 7, 1),
+    },
+}));
+
+export function ArtBoxWide(props) {
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+  
+    const handleOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
+  
+    return (
+      <div className="ArtWide">
+        <div className="ModalClick" onClick={handleOpen}>
+            <img src={props.img} width="450px" height="253px;" />
+        </div>
+        <div className="Artinfo">
+            <p className="atitle">
+                제목 : {props.title}
+            </p>
+            <p className="atitle">
+                프로그램 : {props.program}
+            </p>
+            <p className="atitle">
+                작업기간 : {props.length}
+            </p>
+            <p className="adescp">
+                {props.adesc}
+            </p>
+        </div>
+        <Modal
+          className={classes.modal}
+          open={open}
+          onClose={handleClose}
+          closeAfterTransition
+          BackdropComponent={Backdrop}
+          BackdropProps={{
+            timeout: 100,
+          }}
+        >
+          <Fade in={open}>
+            <div className="ModalBox">
+                <img src={props.modalImg} width="100%" />        
             </div>
-        );
-    }
+          </Fade>
+        </Modal>
+      </div>
+    );
 }
 
-class ArtBoxWide extends Component {
-    render(){
-        return(
-            <div className="ArtWide">
-                <img src={this.props.img} width="450px" />
-                <div>
-                    <p className="atitle">
-                        제목 : {this.props.title}
-                    </p>
-                    <p className="atitle">
-                        프로그램 : {this.props.program}
-                    </p>
-                    <p className="atitle">
-                        작업기간 : {this.props.length}
-                    </p>
-                    <p className="adescp">
-                        {this.props.adesc}
-                    </p>
-                </div>
+export function ArtBoxDefault(props) {
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+  
+    const handleOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
+  
+    return (
+      <div className="ArtBox">
+        <div className="ModalClickS" onClick={handleOpen}>
+            <img src={props.img} width="450px" height="253px;" />
+        </div>
+        <div className="Artinfo">
+            <p className="atitle">
+                제목 : {props.title}
+            </p>
+            <p className="atitle">
+                프로그램 : {props.program}
+            </p>
+            <p className="atitle">
+                작업기간 : {props.length}
+            </p>
+            <p className="adescp">
+                {props.adesc}
+            </p>
+        </div>
+        <Modal
+          className={classes.modal}
+          open={open}
+          onClose={handleClose}
+          closeAfterTransition
+          BackdropComponent={Backdrop}
+          BackdropProps={{
+            timeout: 100,
+          }}
+        >
+          <Fade in={open}>
+            <div className="ModalBoxS">
+                <img src={props.modalImg} width="100%" />
             </div>
-        );
-    }
-}
-
-class ArtBoxWidem extends Component {
-    render(){
-        return(
-            <div className="ArtWide">
-                <img src={this.props.img} width="510px" />
-                <div>
-                    <p className="atitle">
-                        제목 : {this.props.title}
-                    </p>
-                    <p className="atitle">
-                        프로그램 : {this.props.program}
-                    </p>
-                    <p className="atitle">
-                        작업기간 : {this.props.length}
-                    </p>
-                    <p className="adescp">
-                        {this.props.adesc}
-                    </p>
-                </div>
-            </div>
-            
-        );
-    }
-}
-
-class ArtBoxVideo extends Component {
-    render(){
-        return(
-            <div className="ArtVideo">
-                <video  loop controls muted>
-                    <source src={this.props.video} type="video/mp4" />현재 실행중인 브라우저에서는 비디오 태그가 지원되지 않습니다. 브라우저를 업데이트 해주세요.
-                </video>
-                <div>
-                    <p className="atitle">
-                        제목 : {this.props.title}
-                    </p>
-                    <p className="atitle">
-                        프로그램 : {this.props.program}
-                    </p>
-                    <p className="atitle">
-                        작업기간 : {this.props.length}
-                    </p>
-                    <p className="adescp">
-                        {this.props.adesc}
-                    </p>
-                </div>
-            </div>
-        );
-    }
+          </Fade>
+        </Modal>
+      </div>
+    );
 }
 
 class ArtBoxYoutube extends Component {
@@ -176,6 +195,7 @@ function Art() {
                             program={"포토샵, 애프터 이펙트"}
                             length={"2일"}
                             adesc={"우주의 모든 것은 원으로 시작되어 있음을 표현하였습니다."}
+                            modalImg={circles}
                             />
                         </li>
                         <li>
@@ -184,6 +204,7 @@ function Art() {
                             program={"일러스트레이터"}
                             length={"2주"}
                             adesc={"아케이드 게임 Fall Guys: Ultimate Knockout 의 캐릭터가 바나나 의상을 입고 떨어지거나 얇은 다리를 건너는 등, 게임 제목에 맞게 아슬아슬한 연출을 표현하였습니다."}
+                            modalImg={fgpolygonart}
                             />
                         </li>
                         <li>
@@ -200,6 +221,7 @@ function Art() {
                             program={"그림판"}
                             length={"3초"}
                             adesc={"그냥 테스트 이미지 그냥 테스트 이미지 그냥 테스트 이미지 그냥 테스트 이미지 그냥 테스트 이미지"}
+                            modalImg={testimage}
                             />
                         </li>
                     </ul>
@@ -215,6 +237,7 @@ function Art() {
                             program={"3ds Max"}
                             length={"2일"}
                             adesc={'어릴 적 많이 즐겨 했었던 닌텐도 사의 게임을 떠올리며 작업한 시리즈 작품중 첫번째 작품입니다. 마리오 라는 친숙한 게임에서 게이머가 직접 레벨을 제작하고 창의력을 폼낼수 있다는 점에서 신박함을 얻어 작업하였습니다.'}
+                            modalImg={smm}
                             />
                         </li>
                         <li>
@@ -223,6 +246,7 @@ function Art() {
                             program={"3ds Max, 포토샵"}
                             length={"2일"}
                             adesc={"어릴 적 많이 즐겨 했었던 닌텐도 사의 게임을 떠올리며 작업한 시리즈 작품중 두번째 작품입니다. 먹을 것을 좋아하는 커비와 먹을 것을 지키려는 적들을 커비 레벨의 대표적인 초원을 배경으로 표현하였습니다."}
+                            modalImg={kirby}
                             />
                         </li>
                         <li>
@@ -231,6 +255,7 @@ function Art() {
                             program={"3ds Max, 포토샵"}
                             length={"1일"}
                             adesc={"어릴 적 많이 즐겨 했었던 닌텐도 사의 게임을 떠올리며 작업한 시리즈 작품중 마지막 작품입니다. 외지에서 한푼도 없이 살아가는 UFO가 아르바이트를 하면서 돈을 벌며 살아가는 컨셉의 게임입니다, 게임을 하면서 나 또한 어려울 수록 힘내자는 생각을 가지고 작품을 제작하였습니다."}
+                            modalImg={hufo}
                             />
                         </li>
                         <li>
@@ -238,6 +263,7 @@ function Art() {
                             title={"whyrano"}
                             program={"3ds Max"}
                             length={"1일"}
+                            modalImg={whyrano}
                             />
                         </li>
                         <li>
@@ -246,16 +272,18 @@ function Art() {
                             program={"3ds Max"}
                             length={"1일"}
                             adesc={"향기는 언젠가 사라지지만 멀리 퍼져 사람들에게 그 형태를 기억하게 합니다. 독창적이고 어디까지 퍼질지 모르는 향기처럼 사람들에게 기억되고 싶습니다."}
+                            modalImg={distanceofscent}
                             />
                         </li>
                     </ul>
                     <ul>
                         <li>
-                            <ArtBoxWidem img={missing}
+                            <ArtBoxWide img={missing}
                             title={"01. Missing"}
                             program={"3ds Max, 포토샵"}
                             length={"2일"}
                             adesc={"음악을 듣고 떠오르는 장면을 만든 시리즈 첫번째 작품 입니다. 우리는 자신을 모른채 살아가고 있다, 우리는 자신을 찾으려 떠나고 있다."}
+                            modalImg={missing}
                             />
                         </li>
                         <li>
@@ -264,6 +292,7 @@ function Art() {
                             program={"3ds Max, 포토샵"}
                             length={"4일"}
                             adesc={"음악을 듣고 떠오르는 장면을 만든 시리즈 두번째 작품 입니다. 나는 삐에로, 어떤 숲속에서 펼쳐지는 환상의 무대, 위험할지도 모르지만 열심히 해보려한다."}
+                            modalImg={pierrot}
                             />
                         </li>
                         <li>
@@ -272,6 +301,7 @@ function Art() {
                             program={"3ds Max"}
                             length={"2일"}
                             adesc={"고등학생의 마지막 해를 맞아 2019년부터 2021년까지의 작품들의 대부분을 모아, 이제 마지막 종점을 찍는다는 의미로 제작하였습니다. '추억의 기차' 를 타고 작품을 제작하면서의 많은 추억들을 되돌아보고 이제 마지막을 맞이 해야합니다."}
+                            modalImg={tripofmemory}
                             />
                         </li>
                     </ul>
